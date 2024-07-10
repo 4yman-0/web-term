dcd8ec81c69fedbef9db3a05f32870e45b1c3a7f
Ayman El <itcanbeanythingreally@gmail.com>
2024-07-10 13:17:20 +0100

    Revert "Pro Max SUSpicious"
    This reverts commit 03a71e30dc2811e4e68b75e3093338abdfa484ed.

--- js/app.js
+++ js/app.js
@@ -11,8 +11,6 @@ const App = {
         this.termPrompt = document.getElementById("term_prompt");
         this.termCommand = document.getElementById("term_command");
         this.termInput = document.getElementById("term_input");
-        this.termInputDisplay = document.getElementById("term_input_display");
-        this.termInputBlink = document.getElementById("term_input_blink");
 
         // Update config
         Config.updateConfig();
@@ -21,18 +19,13 @@ const App = {
         this.termInput.value = "";
 
         // Add event listener
-        this.termPrompt.addEventListener("click", this.focusInput.bind(this));
         this.termInput.addEventListener("keydown", this.handleInput.bind(this));
-        this.termInput.addEventListener("input", this.displayInput.bind(this));
-        this.termInput.addEventListener("focus", this.focusInput.bind(this));
-        this.termInput.addEventListener("blur", this.blurInput.bind(this));
     },
     handleInput (evt){
         switch (evt.key) {
             case "Enter":
                 Shell.execUserInput(this.termInput.value);
                 this.termInput.value = "";
-                this.termInputDisplay.textContent = "";
                 break;
             case "ArrowUp":
                 Shell.histUp();
@@ -43,20 +36,6 @@ const App = {
             default:
                 break;
         }
-    },
-    displayInput() {
-        this.termInputDisplay.textContent = this.termInput.value;
-    },
-    focusInput (evt){
-        evt.preventDefault();
-        this.termInput.focus();
-    },
-    focusInput() {
-        this.termInputBlink.classList.add('blink-active');
-        this.termInput.focus();
-    },
-    blurInput() {
-        this.termInputBlink.classList.remove('blink-active');
     }
 };
 
