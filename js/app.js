fbe707cb0d9410287e1873454d96670e7e5cbbbd
Ayman El <itcanbeanythingreally@gmail.com>
2024-07-10 13:14:59 +0100

    Revert "Add Bloat and Code like MS"
    This reverts commit 5da1c436dbff285ac76bbfd83340393d91769a94.

--- js/app.js
+++ js/app.js
@@ -8,13 +8,13 @@ const _$ = (id) => document.getElementById(id);
 const App = {
     async init () {
         // Get elements
-        this.term =         _$("term");
-        this.termOutput =   _$("term-output");
-        this.termPrompt =   _$("term-prompt");
-        this.termPS1 =      _$("term-ps1");
-        this.termInput =    _$("term-input-hidden");
+        this.term = _$("term");
+        this.termOutput = _$("term-output");
+        this.termPrompt = _$("term-prompt");
+        this.termPS1 = _$("term-ps1");
+        this.termInput = _$("term-input-hidden");
         this.termInputDisplay = _$("term-input-display");
-        this.termInputBlink =   _$("term-input-blink");
+        this.termInputBlink = _$("term-input-blink");
 
         // Update config
         Config.updateConfig();
@@ -23,12 +23,12 @@ const App = {
         this.termInput.value = "";
 
         // Add event listener
-        this.termPrompt.addEventListener("click",   this.focusInput .bind(this));
-        this.termInput.addEventListener("keydown",  this.handleInput .bind(this));
-        this.termInput.addEventListener("input",    this.updateInput .bind(this));
-        this.termInput.addEventListener("input",    this.updateCursor.bind(this, 0));
-        this.termInput.addEventListener("focus",    this.focusInput .bind(this));
-        this.termInput.addEventListener("blur",     this.blurInput  .bind(this));
+        this.termPrompt.addEventListener("click", this.focusInput.bind(this));
+        this.termInput.addEventListener("keydown", this.handleInput.bind(this));
+        this.termInput.addEventListener("input", this.updateInput.bind(this));
+        this.termInput.addEventListener("input", this.updateCursor.bind(this, 0));
+        this.termInput.addEventListener("focus", this.focusInput.bind(this));
+        this.termInput.addEventListener("blur", this.blurInput.bind(this));
     },
     handleInput (evt){
         switch (evt.key) {
@@ -70,39 +70,42 @@ const App = {
     blurInput() {
         this.termInputBlink.classList.remove('blink-active');
     },
-    updateCursor(offset = 0) {
+    updateCursor (offset = 0){
         const inputLength = this.termInput.value.length;
         let inputCaret = this.termInput.selectionStart;
 
-        if (offset === "begin") {
+        if (offset == "begin") {
             inputCaret = 0;
-        } else if (offset === "end") {
+        } else if (offset == "end") {
             inputCaret = inputLength;
         } else if (typeof offset === "number") {
             inputCaret += offset;
         } else return null;
 
-        // Check if offset is out-of-bound
-        if (inputCaret < 0 || inputCaret > inputLength) {
-            inputCaret -= offset; // revert to previous caret position
-        }
-
+        // check if offset is out-of-bound
+        if (inputCaret === -1 || inputCaret > inputLength) inputCaret -= offset;
+        
         const caretRight = inputLength - inputCaret;
-        this.termInputBlink.style.right = `${caretRight}ch`;
 
-        const isTextSelected = inputCaret !== this.termInput.selectionEnd
-                            && inputCaret !== inputLength;
+        this.termInputBlink
+            .style.right = `${caretRight}ch`;
 
-        if (isTextSelected) {
-            this.termInputBlink.style.width = `${
-                Math.max(inputCaret, this.termInput.selectionEnd) - 
-                Math.min(inputCaret, this.termInput.selectionEnd)
-            }ch`;
-
-            console.debug(inputCaret, this.termInput.selectionEnd, this.termInputBlink.style.width);
+        if (inputCaret !== this.termInput.selectionEnd) {
+            this.termInputBlink
+                .style.width = `${
+                    Math.max(
+                        inputCaret,
+                        this.termInput.selectionEnd
+                    )
+                    -
+                    Math.min(
+                        inputCaret,
+                        this.termInput.selectionEnd
+                    )
+                }ch`;
         } else {
-            this.termInputBlink.style.width = "1ch";
-            console.debug("reset!");
+            this.termInputBlink
+                .style.width = "1ch";
         }
     }
 };
