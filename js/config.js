ef54133ac9565aed15eb2f524f22699d2e2a2eb5
4yman <170770027+4yman-0@users.noreply.github.com>
2024-08-27 15:56:36 +0100

    Add config.js

---
+++ js/config.js
@@ -0,0 +1,21 @@
+'use strict';
+
+import App from './app.js';
+
+const Config = {
+    username: "user",
+    hostname: "web-term",
+    workingDir: "~",
+    updateConfig (){
+        App.termCommand.innerHTML = `
+        <span class="green">
+            ${this.username}@${this.hostname}
+        </span>
+        :
+        <span class="blue">
+            ${this.workingDir}
+        </span>$ `;
+    }
+};
+
+export default Config;
